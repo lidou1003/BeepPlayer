4b9dc9816b9ee36861ae7ad62a3b1f03e70666b9
lidou1003 <mina20050529@gmail.com>
2026-04-30 04:21:10 +0800

    加入專案檔案。

---
+++ frmBeepPlayer.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace BeepPlayer
+{
+    partial class frmBeepPlayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBeepPlayer));
+            this.palMain = new System.Windows.Forms.Panel();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.palMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // palMain
+            // 
+            this.palMain.Controls.Add(this.btn8);
+            this.palMain.Controls.Add(this.btn7);
+            this.palMain.Controls.Add(this.btn6);
+            this.palMain.Controls.Add(this.btn5);
+            this.palMain.Controls.Add(this.btn4);
+            this.palMain.Controls.Add(this.btn3);
+            this.palMain.Controls.Add(this.btn2);
+            this.palMain.Controls.Add(this.btn1);
+            this.palMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.palMain.Location = new System.Drawing.Point(0, 0);
+            this.palMain.Name = "palMain";
+            this.palMain.Size = new System.Drawing.Size(468, 207);
+            this.palMain.TabIndex = 0;
+            // 
+            // btn2
+            // 
+            this.btn2.BackgroundImage = global::BeepPlayer.Properties.Resources._2;
+            this.btn2.Font = new System.Drawing.Font("Viner Hand ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(77, 22);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(55, 154);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Re";
+            this.btn2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.BackgroundImage = global::BeepPlayer.Properties.Resources._3;
+            this.btn3.Font = new System.Drawing.Font("Viner Hand ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(126, 22);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(55, 154);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Mi";
+            this.btn3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.BackgroundImage = global::BeepPlayer.Properties.Resources._4;
+            this.btn4.Font = new System.Drawing.Font("Viner Hand ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(179, 22);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(55, 154);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "Fa";
+            this.btn4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.BackgroundImage = global::BeepPlayer.Properties.Resources._5;
+            this.btn5.Font = new System.Drawing.Font("Viner Hand ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(232, 22);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(55, 154);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "Sol";
+            this.btn5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.BackgroundImage = global::BeepPlayer.Properties.Resources._6;
+            this.btn6.Font = new System.Drawing.Font("Viner Hand ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(283, 22);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(55, 154);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "La";
+            this.btn6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.BackgroundImage = global::BeepPlayer.Properties.Resources._7;
+            this.btn7.Font = new System.Drawing.Font("Viner Hand ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(334, 22);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(55, 154);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "Si";
+            this.btn7.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.BackgroundImage = global::BeepPlayer.Properties.Resources._8;
+            this.btn8.Font = new System.Drawing.Font("Viner Hand ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.ForeColor = System.Drawing.Color.DarkRed;
+            this.btn8.Location = new System.Drawing.Point(385, 22);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(55, 154);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "Do";
+            this.btn8.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.BackgroundImage = global::BeepPlayer.Properties.Resources._1;
+            this.btn1.Font = new System.Drawing.Font("Viner Hand ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(25, 22);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(55, 154);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Do";
+            this.btn1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // frmBeepPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(468, 207);
+            this.Controls.Add(this.palMain);
+            this.Font = new System.Drawing.Font("標楷體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(400, 80);
+            this.Name = "frmBeepPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "簡易電子琴";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBeepPlayer_FormClosing);
+            this.Load += new System.EventHandler(this.frmBeepPlayer_Load);
+            this.SizeChanged += new System.EventHandler(this.frmBeepPlayer_SizeChanged);
+            this.palMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel palMain;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ frmBeepPlayer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace BeepPlayer
+{
+    public partial class frmBeepPlayer : Form
+    {
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int frequency, int duration);
+
+        // 設定音符對應的頻率（C4, D4, E4, F4, G4, A4, B4, C5）
+        int[] freq = { 523, 587, 659, 698, 784, 880, 988, 1046 };
+
+        int initWidth = 0;
+        int initHeight = 0;
+
+        Dictionary<string, Rect> initControl = new Dictionary<string, Rect>();
+
+
+        public frmBeepPlayer()
+        {
+            InitializeComponent();
+            InitializeButton();
+        }
+
+
+        private void InitializeButton()
+        {
+            // 讓btn1~btn8共用同一個事件處理函式
+            btn2.Click += btn1_Click;
+            btn3.Click += btn1_Click;
+            btn4.Click += btn1_Click;
+            btn5.Click += btn1_Click;
+            btn6.Click += btn1_Click;
+            btn7.Click += btn1_Click;
+            btn8.Click += btn1_Click;
+        }
+
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Enabled = false;
+            Beep(freq[btn.TabIndex], 300);
+            btn.Enabled = true;
+        }
+
+        private void frmBeepPlayer_Load(object sender, EventArgs e)
+        {
+            // 紀錄初始視窗大小
+            this.initWidth = this.palMain.Width;
+            this.initHeight = this.palMain.Height;
+
+            // 紀錄控制項的初始位置和大小
+            foreach (Control ctl in this.palMain.Controls)
+            {
+                this.initControl.Add(ctl.Name, new Rect(ctl.Left, ctl.Top,
+                ctl.Width, ctl.Height));
+            }
+        }
+
+
+        /// <summary>
+        /// 當視窗大小改變時，根據初始大小和控制項的初始位置和大小
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmBeepPlayer_SizeChanged(object sender, EventArgs e)
+        {
+            // 紀錄當前視窗的大小
+            double width = this.palMain.Width;
+            double height = this.palMain.Height;
+
+            // 計算寬度和高度的縮放比例
+            double iRatioWith = width / this.initWidth;
+            double iRatioHeight = height / this.initHeight;
+
+            // 根據縮放比例調整控制項的位置和大小
+            foreach (Control ctl in this.palMain.Controls)
+            {
+                ctl.Left = (int)(initControl[ctl.Name].Left * iRatioWith);
+                ctl.Top = (int)(initControl[ctl.Name].Top * iRatioHeight);
+                ctl.Width = (int)(initControl[ctl.Name].Width * iRatioWith);
+                ctl.Height = (int)(initControl[ctl.Name].Height *
+                iRatioHeight);
+            }
+        }
+
+        private void frmBeepPlayer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("確定要關閉應用程式嗎？", "關閉確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true; // 取消關閉
+            }
+        }
+    }
+}
